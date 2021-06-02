883a69e54eb03f7c3b6af78feab4be7b0cf1f5e2
Renzo Carpio <me@renzocarpio.com>
2021-06-02 00:13:34 -0500

    primer commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['Goku']);
+
+    //const handleAdd = () => {
+        //setCategories([ 'HUNTER', ...categories]);
+        //setCategories(cats => [ ...cats, 'HUNTER OTHER']);
+    //}
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={ setCategories } />
+            <hr />
+            <ol>
+                {
+                    categories.map( category => (
+                        <GifGrid 
+                            category = { category } 
+                            key = { category }
+                        />
+                    ))
+                }
+            </ol>
+        </>
+    )
+}
